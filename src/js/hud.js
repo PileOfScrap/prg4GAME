27c380421e0e01da6bdbb7b0d19cda877540e42b
sethd <sethdebruijn@gmail.com>
2026-06-14 15:52:25 +0200

    DO THE THING

--- src/js/hud.js
+++ src/js/hud.js
@@ -1,10 +1,39 @@
 import { Resources } from "./resources.js"
-import { ScreenElement } from 'excalibur'
+import { ScreenElement, Label, Vector } from 'excalibur'
 
 export class UI extends ScreenElement {
     constructor() {
         super()
         
+        this.ammoCounter = new Label({
+            text: '',
+            pos: new Vector(20, 20),
+            scale: new Vector(3, 3)
+        })
+
+        this.scoreCounter = new Label({
+            text: '',
+            pos: new Vector(240, 20),
+            scale: new Vector(3, 3)
+        })
+
+        this.highCounter = new Label({
+            text: '',
+            pos: new Vector(500, 20),
+            scale: new Vector(3, 3)
+        })
+
+        this.addChild(this.ammoCounter)
+        this.addChild(this.scoreCounter)
+        this.addChild(this.highCounter)
+
+
+    }
+
+    onPreUpdate(engine) {
+        this.ammoCounter.text = `BULLETS: ${this.scene.PLAYER.ammo}`
+        this.scoreCounter.text = `SCORE: ${this.scene.score}`
+        this.highCounter.text = `HIGH SCORE: ${this.scene.engine.high}`
     }
 
 }
